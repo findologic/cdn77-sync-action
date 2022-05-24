d582f3986c169234369997da58546aec403cba74
Stefan Obermayer <s.obermayer@findologic.com>
2022-05-24 15:46:37 +0200

    process files in chunks

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,23 @@ DESTINATION="$INPUT_CDN77_USER@$INPUT_CDN77_HOST:$INPUT_DESTINATION_PATH"
 
 echo $DESTINATION
 
+# Start ssh agent and add key
 eval `ssh-agent`
 echo "$INPUT_CDN77_PRIVATE_KEY" | ssh-add -
 
-rsync --dry-run -arzO --out-format="%o %n" --delete -e "ssh -o StrictHostKeyChecking=no" $INPUT_SOURCE_PATH $DESTINATION
+# sync files to CDN77
+FILES_TO_PURGE=($(rsync --dry-run -arzO --out-format="%o %n" --delete -e "ssh -o StrictHostKeyChecking=no" $INPUT_SOURCE_PATH $DESTINATION | cut -d ' ' -f 2))
 
+# Close ssh agent
 eval `ssh-agent -k`
+
+# process files in chunks of the defined limit
+for((i=0; i < ${#FILES_TO_PURGE[@]}; i+=INPUT_CDN77_PURGE_LIMIT))
+do
+  part=( "${FILES_TO_PURGE[@]:i:INPUT_CDN77_PURGE_LIMIT}" )
+
+  # use jq to create JSON array
+  JSON_FILE_ARRAY=$(printf '%s\n' "${part[@]}" | jq -R . | jq -s .)
+  JSON_PAYLOAD="{\"paths\":$JSON_FILE_ARRAY}"
+  echo $JSON_PAYLOAD
+done
