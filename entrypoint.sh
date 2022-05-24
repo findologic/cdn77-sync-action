e9e22ebc4a96629ee441eb87e6af2e592b0f5a1d
Stefan Obermayer <s.obermayer@findologic.com>
2022-05-24 15:19:13 +0200

    introduce ssh-agent

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@
 DESTINATION="$INPUT_CDN77_USER@$INPUT_CDN77_HOST:$INPUT_DESTINATION_PATH"
 
 echo $DESTINATION
-rsync --dry-run -arzO --out-format="%o %n" --delete $INPUT_SOURCE_PATH $DESTINATION
+
+eval `ssh-agent`
+echo "$INPUT_CDN77_PRIVATE_KEY" | ssh-add -
+
+rsync --dry-run -arzO --out-format="%o %n" --delete -e "ssh -o StrictHostKeyChecking=no" $INPUT_SOURCE_PATH $DESTINATION
+
+eval `ssh-agent -k`
