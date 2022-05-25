67e34b7221905643b318f11bb6d54fa907261fb2
Stefan Obermayer <s.obermayer@findologic.com>
2022-05-25 09:20:39 +0200

    add purge request

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,8 @@ do
   part=( "${FILES_TO_PURGE[@]:i:INPUT_CDN77_PURGE_LIMIT}" )
 
   # use jq to create JSON array
-  JSON_FILE_ARRAY=$(printf '%s\n' "${part[@]}" | jq -R . | jq -s .)
+  JSON_FILE_ARRAY=$(printf '%s\n' "/test/${part[@]}" | jq -R . | jq -s .)
   JSON_PAYLOAD="{\"paths\":$JSON_FILE_ARRAY}"
   echo $JSON_PAYLOAD
+  curl https://api.cdn77.com/v3/cdn/$INPUT_CDN77_RESOURCE_ID/job/purge -sS --header "Authorization: Bearer $INPUT_CDN77_API_TOKEN" --data ${JSON_PAYLOAD}
 done
