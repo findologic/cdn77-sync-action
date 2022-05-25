b877dc0d91934d56dbb4c38a7f1bfae0b5969dfa
Stefan Obermayer <s.obermayer@findologic.com>
2022-05-25 12:14:14 +0200

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 DESTINATION="$INPUT_CDN77_USER@$INPUT_CDN77_HOST:$INPUT_DESTINATION_PATH"
 
 # Start ssh agent and add key
-eval `ssh-agent`
+eval "$(ssh-agent)"
 echo "$INPUT_CDN77_PRIVATE_KEY" | ssh-add -
 
 # sync files to CDN77
-FILES_TO_PURGE=($(rsync -arzO --out-format="%o %n" --delete -e "ssh -o StrictHostKeyChecking=no" $INPUT_SOURCE_PATH $DESTINATION | cut -d ' ' -f 2))
+FILES_TO_PURGE=($(rsync -arzO --out-format="%o %n" --delete -e "ssh -o StrictHostKeyChecking=no" "$INPUT_SOURCE_PATH" "$DESTINATION" | cut -d ' ' -f 2))
 
 # Close ssh agent
-eval `ssh-agent -k`
+eval "$(ssh-agent -k)"
 
 # process files in chunks of the defined limit
 for((i=0; i < ${#FILES_TO_PURGE[@]}; i+=INPUT_CDN77_PURGE_LIMIT))
@@ -18,7 +18,7 @@ do
   part=( "${FILES_TO_PURGE[@]:i:INPUT_CDN77_PURGE_LIMIT}" )
 
   # use jq to create JSON array
-  JSON_FILE_ARRAY=$(printf '%s\n' "/test/${part[@]}" | jq -R . | jq -s .)
+  JSON_FILE_ARRAY=$(printf '/test/%s\n' "${part[@]}" | jq -R . | jq -s .)
   JSON_PAYLOAD="{\"paths\":$JSON_FILE_ARRAY}"
 
   # Send purge request to CDN77 API
